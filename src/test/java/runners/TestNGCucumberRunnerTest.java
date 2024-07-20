2a1f75aed1cb3666b83fd7ffffda97564945d07a
Justin Harrison <jth9819@gmail.com>
2024-07-19 19:01:36 -0500

    Make tag changes

--- src/test/java/runners/TestNGCucumberRunnerTest.java
+++ src/test/java/runners/TestNGCucumberRunnerTest.java
@@ -8,12 +8,21 @@ import org.testng.annotations.DataProvider;
         features = "src/test/resources/features",
         glue = {"stepDefinitions"},
         plugin = {"pretty", "html:target/cucumber-reports"},
-        monochrome = true
+        monochrome = true,
+        tags = "@DefaultTag"
 )
 public class TestNGCucumberRunnerTest extends AbstractTestNGCucumberTests {
-    @Override
+
     @DataProvider(parallel = true)
     public Object[][] scenarios() {
         return super.scenarios();
+    }
+
+    static {
+        // Set the tags from system property if provided
+        String tags = System.getProperty("cucumber.filter.tags");
+        if (tags != null && !tags.isEmpty()) {
+            System.setProperty("cucumber.filter.tags", tags);
+        }
     }
 }
--- src/test/resources/features/home.feature
+++
@@ -1,5 +0,0 @@
-Feature: Home Page
-
-  Scenario: Successful login with valid credentials
-    Given the user is on the home page
-    Then the user can see the home page header
